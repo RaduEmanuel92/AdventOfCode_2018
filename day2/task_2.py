ebb7d0b1f345961e16e16678e11227633180642c
RaduEmanuel92 <chiscariuradu@gmail.com>
2018-12-16 01:13:42 +0200

    solved second challenge

--- day2/task_2.py
+++ day2/task_2.py
@@ -3,23 +3,23 @@
 from collections import Counter
 from difflib import SequenceMatcher
 import sys
+
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 def part_1():
 	with open('input.dat', 'r') as ids:
-		i = 0
 		candidates = [0,0]
 		for idx in ids:
 			letters = Counter()
 			for letter in idx:
 				letters[letter] += 1
-			
+	
 			if 2 in letters.values():
 				candidates[0] += 1				
 			if 3 in letters.values():
 				candidates[1] += 1
-
+				
 		print "[+] Found box IDs with two   identical letters: {}".format(candidates[0])
 		print "[+] Found box IDs with three identical letters: {}".format(candidates[1])
 		print "[+] Final checksum: {}".format(candidates[0] * candidates[1])					
